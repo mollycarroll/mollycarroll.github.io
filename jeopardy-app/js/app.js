6f70ba2644dbb9982ad7a65b69b0180ed5b1f13f
Molly Carroll <mollyajcarroll@gmail.com>
2021-02-06 11:43:08 -0600

    Third question, a geography 10 question, is operational

--- jeopardy-app/js/app.js
+++ jeopardy-app/js/app.js
@@ -1,14 +1,13 @@
 /*
 Problems to tackle:
 
-Current behavior -- when HP 1 is answered, no appearance of message on submit, modal does not close, Astronomy 1 box turns blue
+Adding question and answer content in HTML
 
-Player 2's turn -- need another question
-
-Conditions for final round
+Adding functions for each question
 
 */
 
+// GENERIC VARIABLES AND FUNCTIONS
 
 let player1Score = 0;
 let player2Score = 0;
@@ -17,20 +16,77 @@ let turnPlayer = 1;
 
 let roundNumber = 1;
 
-// first astro question
-const openQuestion = () => {
-    document.querySelector('#question-modal-astro-10').style.display = 'block';
+// to switch rounds once turn is over
+const switchPlayer = () => {
+    if (turnPlayer === 1) {
+        turnPlayer = 2;
+        document.querySelector('#turn-player').innerHTML = 'Player 2';
 
-    document.querySelector('#question-form-astro-10').addEventListener('submit', submitAnswer);
+    } else if (turnPlayer === 2) {
+        turnPlayer = 1;
+        document.querySelector('#turn-player').innerHTML = 'Player 1';
+        roundNumber += 1;
+
+        console.log('round updated to round: ' + roundNumber);
+    }
 }
 
-const closeQuestion = () => {
+
+// to add the specified score amount to the player's score
+const addToScore = (num) => {
+    if (turnPlayer === 1) {
+        player1Score = player1Score + num;
+        document.querySelector('#player-1-score').innerHTML = player1Score;
+
+    } else if (turnPlayer === 2) {
+        player2Score += num;
+        document.querySelector('#player-2-score').innerHTML = player2Score;
+    }
+
+}
+
+// QUESTION-SPECIFIC FUNCTIONS
+
+// first astro question
+const openFirstAstroQuestion = () => {
+    document.querySelector('#question-modal-astro-10').style.display = 'block';
+
+    document.querySelector('#question-form-astro-10').addEventListener('submit', submitFirstAstroAnswer);
+}
+
+const closeFirstAstroQuestion = () => {
     document.querySelector('#question-modal-astro-10').style.display = 'none';
 
     document.querySelector('#question-astro-10').style.backgroundColor = '#095CB0';
 
-    document.querySelector('#question-astro-10').removeEventListener('click', openQuestion);
+    document.querySelector('#question-astro-10').removeEventListener('click', openFirstAstroQuestion);
     
+}
+
+
+const submitFirstAstroAnswer = (e) => {
+    e.preventDefault();
+
+    const gas = document.querySelector('#gas');
+
+    // if correct answer is chosen
+    if (gas.checked) {
+        document.querySelector('#update-correct-astro-10').style.display = 'block';
+
+        addToScore(10);
+        
+        setTimeout(closeFirstAstroQuestion, 3000);
+
+    // if incorrect answer is chosen
+    } else if (!gas.checked) {
+
+        document.querySelector('#update-incorrect-astro-10').style.display = 'block';
+
+        setTimeout(closeFirstAstroQuestion, 3000);
+    }
+
+    switchPlayer();
+
 }
 
 // first HP question
@@ -48,71 +104,10 @@ const closeFirstHPQuestion = () => {
     document.querySelector('#question-hp-10').removeEventListener('click', openFirstHPQuestion);
 }
 
-
-// click event listeners for div rectangles
-document.querySelector('#question-astro-10').addEventListener('click', openQuestion);
-
-document.querySelector('#question-hp-10').addEventListener('click', openFirstHPQuestion);
-
-// to switch rounds once turn is over
-const switchPlayer = () => {
-    if (turnPlayer === 1) {
-        turnPlayer = 2;
-        document.querySelector('#turn-player').innerHTML = 'Player 2';
-
-    } else if (turnPlayer === 2) {
-        turnPlayer = 1;
-        document.querySelector('#turn-player').innerHTML = 'Player 1';
-        roundNumber += 1;
-
-        console.log('round updated to round: ' + roundNumber);
-    }
-}
-
-// to add the specified score amount to the player's score
-const addToScore = (num) => {
-    if (turnPlayer === 1) {
-        player1Score = player1Score + num;
-        document.querySelector('#player-1-score').innerHTML = player1Score;
-
-    } else if (turnPlayer === 2) {
-        player2Score += num;
-        document.querySelector('#player-2-score').innerHTML = player2Score;
-    }
-
-}
-
-
-const submitAnswer = (e) => {
-    e.preventDefault();
-
-    const gas = document.querySelector('#gas');
-
-    // if correct answer is chosen
-    if (gas.checked) {
-        document.querySelector('#update-correct-astro-10').style.display = 'block';
-
-        addToScore(10);
-        
-        setTimeout(closeQuestion, 3000);
-
-    // if incorrect answer is chosen
-    } else if (!gas.checked) {
-
-        document.querySelector('#update-incorrect-astro-10').style.display = 'block';
-
-        setTimeout(closeQuestion, 3000);
-    }
-
-    switchPlayer();
-
-}
-
 const submitFirstHPAnswer = (e) => {
     e.preventDefault();
 
     const owl = document.querySelector('#owl');
-    console.log(owl);
 
     if (owl.checked) {
         document.querySelector('#update-correct-hp-10').style.display = 'block';
@@ -131,3 +126,48 @@ const submitFirstHPAnswer = (e) => {
     switchPlayer();
 
 }
+
+// first geo question
+const openFirstGeoQuestion = () => {
+    document.querySelector('#question-modal-geo-10').style.display = 'block';
+
+    document.querySelector('#question-form-geo-10').addEventListener('submit', submitFirstGeoAnswer);
+}
+
+const closeFirstGeoQuestion = () => {
+    document.querySelector('#question-modal-geo-10').style.display = 'none';
+
+    document.querySelector('#question-geo-10').style.backgroundColor = '#095CB0';
+
+    document.querySelector('#question-geo-10').removeEventListener('click', openFirstGeoQuestion);
+}
+
+const submitFirstGeoAnswer = (e) => {
+    e.preventDefault();
+
+    const summer = document.querySelector('#summer');
+
+    if (summer.checked) {
+        document.querySelector('#update-correct-geo-10').style.display = 'block';
+
+        addToScore(10);
+        
+        setTimeout(closeFirstGeoQuestion, 3000);
+
+    } else if (!summer.checked) {
+
+        document.querySelector('#update-incorrect-geo-10').style.display = 'block';
+
+        setTimeout(closeFirstGeoQuestion, 3000);
+    }
+
+    switchPlayer();
+
+}
+
+// click event listeners for div rectangles
+document.querySelector('#question-astro-10').addEventListener('click', openFirstAstroQuestion);
+
+document.querySelector('#question-hp-10').addEventListener('click', openFirstHPQuestion);
+
+document.querySelector('#question-geo-10').addEventListener('click', openFirstGeoQuestion);
